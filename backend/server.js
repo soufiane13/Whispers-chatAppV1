9012cf79815970a8381623b74866cc14d8e8acda
soufiane benhabboul <soufiane0bn@gmail.com>
2025-01-08 02:51:15 +0100

    add routing for authentication, messages, and users; configure cookie parser and static file serving

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,31 @@
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
+import authRoutes from "./routes/auth.r.js";
+import messageRoutes from "./routes/message.r.js";
+import userRoutes from "./routes/user.r.js";
 
 import connectToMongoDB from "./DataBase/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-
-
 dotenv.config();
 
 const __dirname = path.resolve();
-// Assign PORT after calling dotenv.config() to access environment variables.
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
